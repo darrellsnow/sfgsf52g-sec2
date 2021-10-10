76ad793c9c909bd97ae763ee5e4e0c23ea02d788
Darrell A Snow <darrell.snow@mindtrakgolf.com>
2021-10-09 18:01:41 -0700

    Initial Commit

---
+++ src/main/java/com/iengagelabs/sf5b2gsec2/Sf5b2gSec2Application.java
@@ -0,0 +1,13 @@
+package com.iengagelabs.sf5b2gsec2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sf5b2gSec2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sf5b2gSec2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/iengagelabs/sf5b2gsec2/Sf5b2gSec2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iengagelabs.sf5b2gsec2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sf5b2gSec2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
